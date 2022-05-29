114f3f50dd0444a1611f2fd013abca7831ba4f36
seoh2k <seoh2k@gmail.com>
2022-05-29 17:31:32 +0900

    테스트 코드 작성 1. Hello Controller 테스트 코드 작성 2. 롬복 소개 및 설치 3. Hello Controller 코드를 롬복으로 전환

---
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class) // 스프링 부트 테스트와 JUnit 사이에 연결자 역할
+@WebMvcTest(controllers = HelloController.class) // Web(Spring MVC)에 집중할 수 있는 어노테이션으로 @Controller, @ControllerAdvice 등을 사용할 수 있음
+public class HelloControllerTest {
+
+    @Autowired // 스프링이 관리하는 빈(Bean)을 주입받음
+    private MockMvc mvc; // 스프링 MVC 테스트의 시작점으로 HTTP GET, POST 등에 대한 API를 테스트 할 수 있음
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청
+                .andExpect(status().isOk()) // HTTP Header의 Status를 검증, 200인지 아닌지를 검증
+                .andExpect(content().string(hello)); // 응답 본문의 내용을 검증, Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name) // param: API 테스트할 때 사용될 요청 파라미터를 설정, 값은 String만 허용
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // jsonPath: json 응답값을 필드별로 검증할 수 있는 메서드, $을 기준으로 필드명을 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
