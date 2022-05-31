e21e28a4549a4957632f9e2b032d85ba5163bce4
seoh2k <seoh2k@gmail.com>
2022-05-31 13:02:02 +0900

    프로젝트에 Spring Data JPA 적용

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 클래스 내 모든 필드의 빌더 패턴 클래스를 생성
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+
+    @Id // 해당 테이블의 PK필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 기본값 외에 추가로 변경이 필요한 옵션이 있을 때 사용
+    private String titile;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.titile = title;
+        this.content = content;
+        this.author = author;
+    }
+}
