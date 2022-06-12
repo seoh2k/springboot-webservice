1e024378d7864950e109e28ce21f3e369de0c5f5
seoh2k <seoh2k@gmail.com>
2022-06-12 15:45:15 +0900

    머스테치로 화면 구성: 기본페이지 구현

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index"; // 머스테치 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
+    }
+}
