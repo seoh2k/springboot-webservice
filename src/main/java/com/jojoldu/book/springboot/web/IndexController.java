f6ef99f1bb83174c97ff949cf16ae8fe4dae1b99
seoh2k <seoh2k@gmail.com>
2022-06-12 17:33:27 +0900

    머스테치로 화면 구성: 게시글 등록 화면 구현

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.jojoldu.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index"; // 머스테치 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
