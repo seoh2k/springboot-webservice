ad52b083cec8ed9aa7aba83cc40e21b0f450b390
seoh2k <seoh2k@gmail.com>
2022-06-19 23:08:12 +0900

    세션 저장소로 데이터베이스 사용 - gradle에 jdbc 추가 - properties에 jdbc 추가 - 직렬화 오류 발생하여 SessionUser에 implements Serializable 추가하여 해결

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -3,9 +3,11 @@ package com.jojoldu.book.springboot.config.auth.dto;
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 // 직렬화 기능을 가진 세션 Dto
 @Getter
-public class SessionUser { // 인증된 사용자 정보만 필요
+public class SessionUser implements Serializable { // 인증된 사용자 정보만 필요
 
     private String name;
     private String email;
